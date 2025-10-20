0aab5fbe718c1baed7c2b404b03fa41e65a10502
emlanis <46285488+emlanis@users.noreply.github.com>
2025-10-20 21:40:12 +0100

    Initial Commit

---
+++ HausaKeyboard/HausaKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HausaKeyboardApp.swift
+//  HausaKeyboard
+//
+//  Created by emlanis on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HausaKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HausaKeyboardTests/HausaKeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  HausaKeyboardTests.swift
+//  HausaKeyboardTests
+//
+//  Created by emlanis on 20/10/2025.
+//
+
+import Testing
+@testable import HausaKeyboard
+
+struct HausaKeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
